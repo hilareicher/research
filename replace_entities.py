bdce8d245e92f6ca6102cc5e4cd2e8830ca8a871
hilareicher <134700862+hilareicher@users.noreply.github.com>
2023-11-21 15:46:06 +0200

    fix bug in entity replacement

--- replace_entities.py
+++ replace_entities.py
@@ -12,30 +12,32 @@ def load_json_from_file(file):
 
 def replace_entities_in_response():
     for doc in response["docs"]:
-        doc["new_text"] = doc["text"]
-        doc["marked_new_text"] = doc["text"]
+        doc["new_text"] = doc["original_text"]
+        # doc["marked_new_text"] = doc["original_text"]
         replacements = []  # list of replacements for this doc
         for item in doc["items"]:
             for entity in entities:
-                if entity[0] == doc["id"] and entity[1] == item["text"] \
-                        and entity[2] == item["textEntityType"]:
+                if entity[0] == doc["id"] and entity[1] == str(item["textStartPosition"]) \
+                        and entity[2] == item["text"] \
+                        and entity[3] == item["textEntityType"]:
                     # generate text with replaced entity
                     # if no replacement was specified, use the original text
-                    mask = entity[3] if len(entity[3]) > 0 else entity[1]
-                    is_mask_specified = len(entity[3]) > 0
-                    replacements.append({"mask": mask, "maskStartPosition": item["maskStartPosition"],
-                                         "maskEndPosition": item["maskEndPosition"],
+                    mask = entity[4] if len(entity[4]) > 0 else entity[2]
+                    is_mask_specified = len(entity[4]) > 0
+                    replacements.append({"mask": mask, "textStartPosition": item["textStartPosition"],
+                                         "textEndPosition": item["textEndPosition"],
                                          "is_mask_specified": is_mask_specified})
         # apply replacements to text
-        replacements.sort(key=lambda x: -1 * x["maskStartPosition"])  # reverse order
+        replacements.sort(key=lambda x: -1 * x["textStartPosition"])  # reverse order
         for replacement in replacements:
-            doc["new_text"] = doc["new_text"][:replacement["maskStartPosition"]] + \
-                              replacement["mask"] + doc["new_text"][replacement["maskEndPosition"]:]
+            doc["new_text"] = doc["new_text"][:replacement["textStartPosition"]] + \
+                              replacement["mask"] + doc["new_text"][replacement["textEndPosition"]:]
 
             css_class = "replaced" if replacement["is_mask_specified"] else "orig"
-            doc["marked_new_text"] = doc["marked_new_text"][:replacement["maskStartPosition"]] + \
-                                     "<span class=\"" + css_class + "\">" + replacement["mask"] + "</span>" + \
-                                     doc["marked_new_text"][replacement["maskEndPosition"]:]
+            # commenting this out as this is no longer needed
+            # doc["marked_new_text"] = doc["marked_new_text"][:replacement["textStartPosition"]] + \
+            #                          "<span class=\"" + css_class + "\">" + replacement["mask"] + "</span>" + \
+            #                          doc["marked_new_text"][replacement["textEndPosition"]:]
         # print("updated text for doc " + doc["id"] + ": " + doc['new_text'])
 
 
@@ -69,11 +71,22 @@ def save_as_individual_html_files(response, output_dir):
             file.write("<html><head><style>"
                        ".orig { color: blue; }"
                        ".replaced { color: red; }"
-                       "</style><meta charset=\"UTF-8\"></head><body>")
+                       "</style><meta charset=\"UTF-8\"></head><body><pre>")
             file.write(doc["marked_new_text"])
-            file.write("</body></html>")
+            file.write("</pre></body></html>")
         docs += 1
     print("saved " + str(docs) + " html docs")
+
+
+def add_orig_text_to_safeheb_response():
+    # load original input files
+    files = list(pathlib.Path(input_dir).glob('*'))
+    for doc in response["docs"]:
+        for f in files:
+            if doc["id"] == f.name: # found original input file
+                with open(f, 'r') as file:
+                    orig_text = file.read()
+                    doc["original_text"] = orig_text
 
 
 # load safe harbor response and entities file
@@ -86,6 +99,8 @@ entities_file = os.path.join(input_dir, "entities.csv")
 print("loading identified entities, file: " + entities_file)
 entities = load_csv_from_file(entities_file)
 print("found " + str(len(entities)) + " entities")
+
+add_orig_text_to_safeheb_response()
 
 # perform replacements
 print("generating docs with replacements")
@@ -107,7 +122,7 @@ save_to_file(response, response_file)
 print("saved anonymized docs, file: " + response_file)
 
 print("saving anonymized docs, path: " + output_dir)
-# save_as_individual_text_files(response, output_dir)
-save_as_individual_html_files(response, output_dir)
+save_as_individual_text_files(response, output_dir)
+#save_as_individual_html_files(response, output_dir)
 
 print("done")
